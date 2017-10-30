bbc1fd527543c1b00b8bb95283f6be0af4a70e30
zCam_VFXPC <camglegg@gmail.com>
2017-10-29 21:19:40 -0700

    Version 1 - Not much to say here. Version 2 - Added some code for a dice roller

--- Book_Tests.py
+++ Book_Tests.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import random
+
+roll = random.randint(1,6)
+
+def main():
+    print(roll)
+    ## print("Would you like to roll again? Press Enter.")
+    ## if input():
+        ## print(roll)
+    ## else:
+        ## print('Thanks, see you next time.')
+
+main()
