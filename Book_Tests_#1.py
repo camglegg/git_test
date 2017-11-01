65d4822713b8d48f9f55f42217754ee0df9b8a7f
zCam_VFXPC <camglegg@gmail.com>
2017-10-31 20:50:03 -0700

    Version 1 - Not much to say here. Version 2 - Added some code for a dice roller Version 3 - Removed old Dice rolling Code. Added Tut#1 From ATBS

---
+++ Book_Tests_#1.py
@@ -0,0 +1,8 @@
+# This program says "Hello" and asks for my name.
+
+print("Hello World!")
+# Changed this to assign the variable while simultaneously asking for the input.
+my_name = input("What is your name? ")
+print('It is nice to meet you, ' + my_name)
+print('The length of your name is : ')
+print(len(my_name))
