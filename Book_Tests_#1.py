9163f637a30f1ae7515f3ffad346f9f312070d18
zCam_VFXPC <camglegg@gmail.com>
2017-10-31 20:55:04 -0700

    Version 1 - Not much to say here. Version 2 - Added some code for a dice roller Version 3 - Removed old Dice rolling Code. Added Tut#1 From ATBS

--- Book_Tests_#1.py
+++ Book_Tests_#1.py
@@ -6,3 +6,7 @@ my_name = input("What is your name? ")
 print('It is nice to meet you, ' + my_name)
 print('The length of your name is : ')
 print(len(my_name))
+my_age = input('What is your age? ')
+# This needs the "str" and "int" or it breaks.
+print('You will be ' + str(int(my_age)+1) + ' in a year.')
+
