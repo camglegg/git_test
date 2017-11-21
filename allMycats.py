582d0fbe7bbfdc78ea1a6b8e41572bba92fff86a
cglegg <camglegg@gmail.com>
2017-11-20 21:48:19 -0800

    Version 2 of cat names. Pretty Cool!

--- allMycats.py
+++ allMycats.py
@@ -1,14 +1,10 @@
-print("Enter the name of Cat 1: ")
-catName1 = input()
-print("Enter the name of Cat 2: ")
-catName2 = input()
-print("Enter the name of Cat 3: ")
-catName3 = input()
-print("Enter the name of Cat 4: ")
-catName4 = input()
-print("Enter the name of Cat 5: ")
-catName5 = input()
-print("Enter the name of Cat 6: ")
-catName6 = input()
-print('The cat names are: ')
-print(catName1+', '+catName2+', '+catName3+', '+catName4+', '+catName5+', '+catName6+'.')
+catNames = []
+while True:
+    print('Enter the name of a cat '+str(len(catNames)+1)+ ' (Or Enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break
+    catNames = catNames + [name] # list concatenation
+print("The cat's names are: ")
+for name in catNames:
+    print(' ' + name)
