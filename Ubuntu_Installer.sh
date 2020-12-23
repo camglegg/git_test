32c06c635cb9d912e4040aaca949d1c305216225
camglegg <camglegg@gmail.com>
2020-12-23 09:28:05 -0800

    Create Ubuntu_Installer.sh
    New Ubuntu first time setup installer

---
+++ Ubuntu_Installer.sh
@@ -0,0 +1,27 @@
+#Update
+sudo apt-get update
+#Upgrade
+sudo apt-get upgrade
+# Install repository configuration
+curl -sSL https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft-prod.list
+# Install Microsoft GPG public key
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+# Update package index files
+sudo apt-get update
+#Install Brave Browser
+sudo apt-get install brave-y
+#Install Teams
+sudo apt install teams -y
+#Install Edge
+sudo apt install microsoft-edge-dev -y
+#Install Pip
+sudo apt-get install python3-pip -y
+#Install VSCode
+#Install Unity-Tweak Tool
+sudo apt-get install unity-tweak-tool -y
+#Install Spotify
+sudo apt-get install spotify -y
+#Install Discord
+sudo apt-get install discord -y
+#Install VLC
+sudo apt-get install vlc -y
